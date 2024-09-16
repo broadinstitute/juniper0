34bb167d96639e384c40f29a6c9340389923f2a7
Ivan Specht <ispecht@college.harvard.edu>
2024-09-16 14:24:29 -0700

    debugging code in accept/reject

--- R/subroutines.R
+++ R/subroutines.R
@@ -890,14 +890,7 @@ accept_or_reject <- function(prop, mcmc, data, update, hastings = 0, check_parsi
   #   }
   # }
 
-  ## Check that no SNVs are listed in "dropout"
-  for (i in 1:prop$n) {
-    if(any(
-      prop$subs$pos[[i]] %in% prop$dropout[[i]]
-    )){
-      stop("No mutations should be listed at positions that drop out")
-    }
-  }
+
 
   if(length(check_parsimony) > 0){
     for (k in check_parsimony) {
@@ -906,6 +899,16 @@ accept_or_reject <- function(prop, mcmc, data, update, hastings = 0, check_parsi
       ){
         return(mcmc)
       }
+    }
+  }
+
+  ## Check that no SNVs are listed in "dropout"
+  for (i in 1:prop$n) {
+    if(any(
+      prop$subs$pos[[i]] %in% prop$dropout[[i]]
+    )){
+      print(i)
+      stop("No mutations should be listed at positions that drop out")
     }
   }
 
